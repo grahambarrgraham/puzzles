fd485e94bed5e080dc3e92f518d2cdbd4e3ae4c0
grahamb <rrabarg2@yahoo.com.sg>
2018-06-05 08:44:11 +0100

    started implementation

--- src/main/java/rrabarg/puzzles/cheapesttabcomplete/CheapestTabComplete.java
+++ src/main/java/rrabarg/puzzles/cheapesttabcomplete/CheapestTabComplete.java
@@ -1,9 +1,51 @@
 package rrabarg.puzzles.cheapesttabcomplete;
 
+import lombok.Value;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class CheapestTabComplete {
 
+    TreeNode prefixTree = null;
+
     public int getFewest(String[] names, String w) {
-        return 0;
+
+        Arrays.stream(names).forEach(n -> addName(n));
+        String[] prefixes = getPrefixes(w);
+
+        int currentBest = w.length() + 1;
+
+        for(int tapPressCount = 1; tapPressCount < prefixes.length - 1; tapPressCount++) {
+            int keyPresses = (w.length() - prefixes[tapPressCount - 1].length()) + 1 + tapPressCount;
+            if (keyPresses < currentBest) {
+                currentBest = keyPresses;
+            }
+        }
+
+        return currentBest;
+    }
+
+    void addName(String name) {
+        if (prefixTree == null) {
+            prefixTree = new TreeNode(null,"name", new ArrayList<>());
+        }
+    }
+
+    String[] getPrefixes(String term) {
+        return new String[] {term};
+    }
+
+
+    @Value
+    class TreeNode {
+        TreeNode parent;
+        String prefix;
+        List<TreeNode> children;
     }
 }
+
+
+
 
