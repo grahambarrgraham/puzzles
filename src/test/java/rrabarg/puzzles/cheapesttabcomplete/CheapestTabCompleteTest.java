2611308a95e8117e1bdd9126b48b376e1648ed3b
grahamb <rrabarg2@yahoo.com.sg>
2018-06-08 16:52:21 +0100

    added tab complete system tests

--- src/test/java/rrabarg/puzzles/cheapesttabcomplete/CheapestTabCompleteTest.java
+++ src/test/java/rrabarg/puzzles/cheapesttabcomplete/CheapestTabCompleteTest.java
@@ -1,6 +1,12 @@
 package rrabarg.puzzles.cheapesttabcomplete;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -39,8 +45,31 @@ class CheapestTabCompleteTest {
         assertKeypresses("aaaaaaaaaaaaaaaaaaaaaaa", 21, "aaaa", "aaaa", "aaaa", "aaaa", "aaaa");
     }
 
-    void assertKeypresses(String searchTerm, int expectedKeypresses, String... names) {
+    @Test
+    void case7() {
+        assertKeypresses("buzzzc", 7, "b", "bus", "buz", "buzz", "buzzz");
+    }
 
+    @Test
+    void case8() {
+        assertKeypresses("aaad", 3, "aaaa", "aaad");
+    }
+
+    @Test
+    void case9() {
+        assertKeypresses("bcdefghijk", 9, "bcd", "bc", "bcdef", "bcdefgh", "a");
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/cheapestTabComplete.csv")
+    public void systemTests(String nameArray, String term, int expected) {
+        String[] names = nameArray.split(", ");
+        List<String> collect = Arrays.stream(names).map(a -> a.trim()).filter(a -> a.length() > 0).collect(Collectors.toList());
+        assertKeypresses(term, expected, collect.toArray(new String[collect.size()]));
+    }
+
+
+    void assertKeypresses(String searchTerm, int expectedKeypresses, String... names) {
         assertThat(system.getFewest(names, searchTerm), is(expectedKeypresses));
     }
 }
